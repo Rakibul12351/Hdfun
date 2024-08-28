2fa5a75294ede86fa4631cbe4d5254a1a23d8b7f
Rakibul12351 <mixmix.net200@gmail.com>
2024-08-29 02:30:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function spin() {
-    const spinner = document.getElementById('spinner');
-    const randomDeg = Math.floor(Math.random() * 360) + 360;
-    spinner.style.transition = 'transform 1s ease-out';
-    spinner.style.transform = `rotate(${randomDeg}deg)`;
-}
